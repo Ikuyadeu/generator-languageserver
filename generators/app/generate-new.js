14550c7674b1afb56b6252df73637c0aa7a834d8
Yuki Ueda <ikuyadeu0513@gmail.com>
2022-11-27 23:28:09 +0900

    version 0.0.1

--- generators/app/generate-new.js
+++ generators/app/generate-new.js
@@ -26,18 +26,25 @@ module.exports = {
         generator.fs.copy(generator.templatePath('.vscode'), generator.destinationPath('.vscode'));
         generator.fs.copyTpl(generator.templatePath('package.json'), generator.destinationPath('package.json'), languageServerConfig);
         generator.fs.copyTpl(generator.templatePath('tsconfig.json'), generator.destinationPath('tsconfig.json'), languageServerConfig);
+        generator.fs.copyTpl(generator.templatePath('tsconfig.base.json'), generator.destinationPath('tsconfig.base.json'), languageServerConfig);
         generator.fs.copyTpl(generator.templatePath('.vscodeignore'), generator.destinationPath('.vscodeignore'), languageServerConfig);
         generator.fs.copyTpl(generator.templatePath('shared.webpack.config.js'), generator.destinationPath('shared.webpack.config.js'), languageServerConfig);
+        generator.fs.copyTpl(generator.templatePath('README.md'), generator.destinationPath('README.md'), languageServerConfig);
+        generator.fs.copyTpl(generator.templatePath('CHANGELOG.md'), generator.destinationPath('CHANGELOG.md'), languageServerConfig);
+        generator.fs.copyTpl(generator.templatePath('.eslintrc.base.json'), generator.destinationPath('.eslintrc.base.json'));
 
         if (languageServerConfig.gitInit) {
             generator.fs.copy(generator.templatePath('.gitignore'), generator.destinationPath('.gitignore'));
         }
-        generator.fs.copyTpl(generator.templatePath('README.md'), generator.destinationPath('README.md'), languageServerConfig);
-        generator.fs.copyTpl(generator.templatePath('CHANGELOG.md'), generator.destinationPath('CHANGELOG.md'), languageServerConfig);
-        generator.fs.copy(generator.templatePath('.eslintrc.base.json'), generator.destinationPath('.eslintrc.base.json'));
 
         generator.fs.copy(generator.templatePath('client'), generator.destinationPath('client'));
-        generator.fs.copy(generator.templatePath('server'), generator.destinationPath('server'));
+        generator.fs.copy(generator.templatePath('server/src'), generator.destinationPath('server/src'));
+        generator.fs.copyTpl(generator.templatePath('server/package.json'), generator.destinationPath('server/package.json'), languageServerConfig);
+        generator.fs.copyTpl(generator.templatePath('server/tsconfig.json'), generator.destinationPath('server/tsconfig.json'), languageServerConfig);
+        generator.fs.copyTpl(generator.templatePath('server/.eslintrc.json'), generator.destinationPath('server/.eslintrc.json'));
+        generator.fs.copyTpl(generator.templatePath('server/.eslintignore'), generator.destinationPath('server/.eslintignore'));
+        generator.fs.copyTpl(generator.templatePath('server/webpack.config.js'), generator.destinationPath('server/webpack.config.js'));
+        generator.fs.copyTpl(generator.templatePath('server/bin/sample-server'), generator.destinationPath(`server/bin/${languageServerConfig.serverName}`));
         languageServerConfig.installDependencies = true;
     }
 }
